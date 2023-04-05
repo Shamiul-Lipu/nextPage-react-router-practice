ac37aad9ced72988cdd688d5ef378e91aaf2455a
unknown <shamiullipu@gmail.com>
2023-04-06 02:47:02 +0600

    route defined and Header section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Outlet } from "react-router-dom"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
 function App() {
   return (
     <div className='App'>
-      <h1>Welcome to nextPage!</h1>
+      {/* Header section */}
+      <Header></Header>
+      {/* Outlet */}
+      <Outlet />
+      {/* Footer section */}
+      <Footer></Footer>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Footer from './components/Footer'
+import Books from './components/Books'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App></App>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>,
+            },
+            {
+                path: 'about',
+                element: <About></About>
+            },
+            {
+                path: 'books',
+                element: <Books></Books>
+            },
+            {
+                path: 'footer',
+                element: <Footer></Footer>
+            }
+        ]
+    }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router}></RouterProvider>
+)
